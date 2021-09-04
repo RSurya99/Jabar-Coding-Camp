84930b58461d4f3fa3d03af516892123171b6e41
RSurya99 <rafliazz679@gmail.com>
2021-09-04 10:10:24 +0700

    Kuis 1 - Function Penghitung Jumlah Kata

---
+++ Quiz-1/index.js
@@ -0,0 +1,21 @@
+// Kuis 1
+// Judul : Function Penghitung Jumlah Kata
+
+// Buatlah sebuah function dengan nama jumlah_kata() yang menerima sebuah kalimat (string), dan mengembalikan nilai jumlah kata dalam kalimat tersebut.
+
+// Contoh
+
+var kalimat_1 = " Halo nama saya Muhammad Iqbal Mubarok "
+var kalimat_2 = "Saya Iqbal"
+
+// jumlah_kata(kalimat_1) // 6
+// jumlah_kata(kalimat_2) // 2
+
+// Jawaban Kuis 1
+function jumlah_kata(string){
+    var splitted = string.split(' ').filter((word) => { if (word !== '') return word;  })
+    return splitted.length;
+}
+
+var result = jumlah_kata(kalimat_1) // 6
+console.log('JAWABAN KUIS 1 : ' + result + '\n');
