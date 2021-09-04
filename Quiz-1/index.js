65c19f282672dfc3393a753267c1357e7e670383
RSurya99 <rafliazz679@gmail.com>
2021-09-04 10:49:31 +0700

    Quiz-1 Function Penghasil Tanggal Hari Esok

--- Quiz-1/index.js
+++ Quiz-1/index.js
@@ -21,3 +21,41 @@ var result = jumlah_kata(kalimat_1) // 6
 console.log('JAWABAN KUIS 1 : ' + kalimat_1 + '= ' + result + '\n');
 
 
+// Kuis 2
+// Judul : Function Penghasil Tanggal Hari Esok
+// Buatlah sebuah function dengan nama next_date() yang menerima 3 parameter tanggal, bulan, tahun dan mengembalikan nilai tanggal hari esok dalam bentuk string, dengan contoh input dan otput sebagai berikut.
+
+// contoh 1
+// var tanggal = 29
+// var bulan = 2
+// var tahun = 2020
+
+// next_date(tanggal , bulan , tahun ) // output : 1 Maret 2020
+
+// contoh 2
+// var tanggal = 28
+// var bulan = 2
+// var tahun = 2021
+
+// next_date(tanggal , bulan , tahun ) // output : 1 Maret 2021
+
+// contoh 3
+var tanggal = 31
+var bulan = 12
+var tahun = 2020
+
+// next_date(tanggal , bulan , tahun ) // output : 1 Januari 2021
+
+// Jawaban Kuis 2
+function next_date(tanggal, bulan, tahun){
+    var months = [ "January", "February", "March", "April", "May", "June", 
+           "July", "August", "September", "October", "November", "December" ];
+    var selectedMonthName = months[bulan-1];
+    var combine = selectedMonthName + '/' + tanggal.toString() + '/' + tahun.toString();
+    var d = new Date(combine);
+    d.setDate(d.getDate() + 1);
+    return `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
+}
+
+var result2 = next_date(tanggal, bulan, tahun);
+console.log('JAWABAN KUIS 2 : ' + result2);
